010c96855fcfdf45b60e7eb3b44d46a58887ae6f
minhttran <trcongminh.261002@gmail.com>
2024-07-18 14:59:57 +0700

    Maketing skeleton

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
+            header
+        </header>
+    )
+}
+
+export default Header
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <Button>Click me</Button>
+      Marketing page
     </div>
   )
 }
