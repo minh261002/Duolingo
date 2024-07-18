7afe0c47834c8498e85e0ba5a7a5cc21dda7e5aa
minhttran <trcongminh.261002@gmail.com>
2024-07-18 15:50:56 +0700

    Authentication

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,9 +1,38 @@
+import { Button } from '@/components/ui/button'
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
     return (
         <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
-            header
+            <div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full'>
+                <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+                    <Image src='/logo.svg' alt='logo' width={40} height={40} />
+                    <h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+                        Duolingo
+                    </h1>
+                </div>
+
+                <ClerkLoading>
+                    <Loader className='h-5 w-5 text-muted-foreground animate-spin' />
+                </ClerkLoading>
+
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton mode='modal'>
+                            <Button size='lg' variant='ghost'>
+                                Sign In
+                            </Button>
+                        </SignInButton>
+
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
         </header>
     )
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,52 @@
 import { Button } from '@/components/ui/button'
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs'
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Marketing page
+    <div className='max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2'>
+      <div className='relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0'>
+        <Image src='/hero.svg' fill alt='hero banner' />
+      </div>
+
+      <div className='flex flex-col items-center gap-y-8'>
+        <h1 className='text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center'>
+          Learn, practice, and master languages on Duolingo.
+        </h1>
+
+        <div>
+          <ClerkLoading>
+            <Loader className='w-5 h-5 text-muted-foreground animate-spin' />
+          </ClerkLoading>
+
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode='modal'>
+                <Button size='lg' variant='secondary' className='w-full'>
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode='modal'>
+                <Button size='lg' variant='primaryOutline' className='w-full'>
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+              <Button size='lg' variant='secondary' className='w-full' asChild>
+                <Link href='/learn'>
+                  Continue Learning
+                </Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   )
 }
